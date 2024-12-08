a37a9ffdf9461b00d64fff6ba319214334a8ca89
brian-wham <brian.wham@techneware.net>
2024-12-08 10:03:43 -0500

    Add Day7

---
+++ AdventOfCode2024/Puzzles/Day7.cs
@@ -0,0 +1,243 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+using static AdventOfCode2024.Puzzles.Day7.Calibration;
+
+namespace AdventOfCode2024.Puzzles
+{
+    /// <summary>
+    /// Bridge Repair
+    /// </summary>
+    /// <see cref="https://adventofcode.com/2024/day/7"/>
+    internal class Day7 : Puzzle
+    {
+        private List<Calibration> Calibrations { get; set; } = [];
+        public Day7()
+            : base(Name: "Bridge Repair", DayNumber: 7) { }
+
+        public override void ParseData()
+        {
+            Calibrations.Clear();
+
+            var data = DataRaw.Replace("\r", "").Split("\n", StringSplitOptions.RemoveEmptyEntries);
+            foreach (var line in data)
+            {
+                var d = line.Split(':', StringSplitOptions.TrimEntries);
+                var value = long.Parse(d[0]);
+                var nums = d[1].Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                                .Select(i => int.Parse(i))
+                                .ToList();
+
+                Calibrations.Add(new Calibration(value, nums));
+            }
+        }
+        public override void Part1(bool isTestMode)
+        {
+            ParseData();
+            foreach (var c in Calibrations)
+            {
+                c.GenerateSolutions([
+                    Calibration.Operator.Add,
+                    Calibration.Operator.Multiply]);
+            }
+
+            Part1Result = GenerateAnswer(isTestMode);
+        }
+        public override void Part2(bool isTestMode)
+        {
+            ParseData();
+
+            foreach (var c in Calibrations)
+            {
+                c.GenerateSolutionsThreaded([
+                    Calibration.Operator.Add,
+                    Calibration.Operator.Multiply,
+                    Calibration.Operator.Concat]);
+            }
+
+            Part2Result = GenerateAnswer(isTestMode);
+        }
+        private string GenerateAnswer(bool isTestMode)
+        {
+            var vc = Calibrations.Where(c => c.IsValid).ToList();
+            var answer = new StringBuilder();
+            if (isTestMode || WithLogging)
+            {
+                foreach (var c in vc)
+                {
+                    answer.Append($"{c.TestValue} Solutions:\n");
+                    foreach (var s in c.Solutions)
+                        answer.Append($" => {s}\n");
+                    answer.Append('\n');
+                }
+            }
+
+            var sum = vc.Sum(c => c.TestValue);
+            answer.Append($"Total Calibration = {sum}\n");
+            return answer.ToString();
+        }
+        internal class Calibration
+        {
+            public enum Operator
+            {
+                None,
+                Add,
+                Multiply,
+                Concat
+            }
+            public long TestValue { get; private set; }
+            public List<int> Numbers { get; private set; }
+            public List<string> Solutions { get; private set; } = [];
+            public bool IsValid => Solutions.Count > 0;
+            public Calibration(long testValue, List<int> numbers)
+            {
+                TestValue = testValue;
+                Numbers = numbers;
+            }
+            internal void GenerateSolutions(List<Operator> operators)
+            {
+                var possibleEquations = GenerateEquations(Numbers, operators);
+                var validSolutions = new List<List<(int, Operator)>>();
+                foreach (var pe in possibleEquations)
+                {
+                    var value = RunEquation(pe);
+                    if (value == TestValue)
+                        validSolutions.Add(pe);
+                }
+
+                SaveSolutions(validSolutions);
+            }
+            internal void GenerateSolutionsThreaded(List<Operator> operators)
+            {
+                var possibleEquations = GenerateEquations(Numbers, operators);
+                var validSolutions = new ConcurrentStack<List<(int, Operator)>>();
+                Parallel.ForEach(possibleEquations, (pe) =>
+                {
+                    var value = RunEquation(pe);
+                    if (value == TestValue)
+                        validSolutions.Push(pe);
+                });
+
+                SaveSolutions([.. validSolutions]);
+            }
+            private void SaveSolutions(List<List<(int, Operator)>> validSolutions)
+            {
+                foreach (var vs in validSolutions)
+                {
+                    string solution = "";
+                    Operator nextOp = Operator.None;
+                    for (int i = 0; i < vs.Count; i++)
+                    {
+                        var (num, op) = vs[i];
+
+                        if (i == 0)
+                        {
+                            solution = $"{num}";
+                        }
+                        else
+                        {
+                            switch (nextOp)
+                            {
+                                case Operator.Add:
+                                    solution += $" + {num}";
+                                    break;
+                                case Operator.Multiply:
+                                    solution += $" * {num}";
+                                    break;
+                                case Operator.Concat:
+                                    solution += $" || {num}";
+                                    break;
+                                case Operator.None:
+                                default:
+                                    break;
+                            }
+                        }
+                        nextOp = op;
+                    }
+
+                    Solutions.Add(solution);
+                }
+            }
+            private static long RunEquation(List<(int num, Operator op)> operations)
+            {
+                long result = 0;
+                Operator nextOp = Operator.None;
+
+                for (int i = 0; i < operations.Count; i++)
+                {
+                    var (num, op) = operations[i];
+
+                    if (i == 0)
+                    {
+                        result = num;
+                    }
+                    else
+                    {
+                        switch (nextOp)
+                        {
+                            case Operator.Add:
+                                result += num;
+                                break;
+                            case Operator.Multiply:
+                                result *= num;
+                                break;
+                            case Operator.Concat:
+                                //Equivelent to => result = long.Parse($"{result}{num}");
+                                //but faster
+                                int numDigits = (int)Math.Log10(num) + 1;
+                                result = result * (long)Math.Pow(10, numDigits) + num;
+                                break;
+                            case Operator.None:
+                            default:
+                                break;
+                        }
+                    }
+                    nextOp = op;
+                }
+
+                return result;
+            }
+            internal static List<List<(int, Operator)>> GenerateEquations(List<int> numbers, List<Operator> operators)
+            {
+                var results = new List<List<(int, Operator)>>();
+                GetPossibleEquations(numbers, operators, [], results);
+                return results;
+            }
+            internal static void GetPossibleEquations(
+                                    List<int> numbers,
+                                    List<Operator> operators,
+                                    List<(int, Operator)> current,
+                                    List<List<(int, Operator)>> results)
+            {
+                int index = current.Count;
+
+                if (index == numbers.Count)
+                {
+                    results.Add(new List<(int, Operator)>(current));
+                    return;
+                }
+
+                if (index == numbers.Count - 1)
+                {
+                    // The last number always has a `None` operator.
+                    current.Add((numbers[index], Operator.None));
+                    GetPossibleEquations(numbers, operators, current, results);
+                    current.RemoveAt(current.Count - 1);
+                }
+                else
+                {
+                    foreach (var op in operators)
+                    {
+                        current.Add((numbers[index], op));
+                        GetPossibleEquations(numbers, operators, current, results);
+                        current.RemoveAt(current.Count - 1);
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ AdventOfCode2024/Tools/PermutationGenerator.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode2024.Tools
+{
+    public static class PermutationGenerator
+    {
+        // Method to generate all permutations of a given list
+        public static List<List<T>> GetPermutations<T>(List<T> list)
+        {
+            var result = new List<List<T>>();
+            Permute(list, 0, result);
+            return result;
+        }
+
+        // Recursive helper method to generate permutations
+        private static void Permute<T>(List<T> list, int startIndex, List<List<T>> result)
+        {
+            if (startIndex == list.Count)
+            {
+                result.Add(new List<T>(list));
+            }
+            else
+            {
+                for (int i = startIndex; i < list.Count; i++)
+                {
+                    Swap(list, startIndex, i);
+                    Permute(list, startIndex + 1, result);
+                    Swap(list, startIndex, i); // Backtrack
+                }
+            }
+        }
+
+        // Swap two elements in a list
+        private static void Swap<T>(List<T> list, int i, int j)
+        {
+            T temp = list[i];
+            list[i] = list[j];
+            list[j] = temp;
+        }
+
+        public static List<List<T>> GetAllCombinationsAndPermutationsOfSize<T>(List<T> items, int minSize, int maxSize)
+        {
+            var result = new List<List<T>>();
+
+            if (items.Count < maxSize)
+            {
+                var idx = 0;
+                var dupes = new List<T>(items);
+                while (items.Count < maxSize)
+                {
+                    items.Add(dupes[idx++]);
+                    if (idx >= dupes.Count)
+                        idx = 0;
+                }
+            }
+
+            // Generate all combinations for all sizes from 1 to the full size of the list
+            for (int size = minSize; size <= maxSize; size++)
+            {
+                // Generate all combinations of a given size
+                var subsets = GetCombinationsOfSize(items, size);
+                foreach (var subset in subsets)
+                {
+                    // For each subset, generate all permutations and add them to the result
+                    var permutations = GetPermutations(subset);
+                    result.AddRange(permutations);
+                }
+            }
+
+            return result;
+        }
+        // Method to generate all combinations (subsets) of the set of all lengths
+        public static List<List<T>> GetAllCombinationsAndPermutations<T>(List<T> items)
+        {
+            var result = new List<List<T>>();
+
+            // Generate all combinations for all sizes from 1 to the full size of the list
+            for (int size = 1; size <= items.Count; size++)
+            {
+                // Generate all combinations of a given size
+                var subsets = GetCombinationsOfSize(items, size);
+                foreach (var subset in subsets)
+                {
+                    // For each subset, generate all permutations and add them to the result
+                    var permutations = GetPermutations(subset);
+                    result.AddRange(permutations);
+                }
+            }
+
+            return result;
+        }
+
+        // Method to generate combinations of a given size
+        private static List<List<T>> GetCombinationsOfSize<T>(List<T> items, int size)
+        {
+            var result = new List<List<T>>();
+            GenerateCombinations(items, size, 0, new List<T>(), result);
+            return result;
+        }
+
+        // Recursive helper method to generate combinations
+        private static void GenerateCombinations<T>(List<T> items, int size, int index, List<T> current, List<List<T>> result)
+        {
+            if (current.Count == size)
+            {
+                result.Add(new List<T>(current));
+                return;
+            }
+
+            for (int i = index; i < items.Count; i++)
+            {
+                current.Add(items[i]);
+                GenerateCombinations(items, size, i + 1, current, result);
+                current.RemoveAt(current.Count - 1);
+            }
+        }
+    }
+}
